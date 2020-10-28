819afd223b809fb0f44965aae8cd95d37bb07117
Victoria <vicglee1234007@gmail.com>
2020-10-27 23:46:09 -0500

    Added updating score according to when virus is killed

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,6 +9,10 @@ import { accelerate, decelerate } from "../utils";
 let box;
 let cursors;
 let pointer;
+var score = 0;
+var scoreText;
+let gameOver = false;
+
 
 export default new Phaser.Class({
   Extends: Phaser.Scene,
@@ -25,6 +29,9 @@ export default new Phaser.Class({
 
     this.add.image(275, 410, "background"); //note: All Phaser3 Game Obj are positioned based on their center by default, and can be changed to have the drawing position set to the top-left by appending .setOrigin(0,0) to this line
 
+    //Score keeping
+    scoreText = this.add.text(20,20, "Score: 0", {fontSize: "30px", fill: "#000"})
+
     // stars.children.iterate(function (child) {
     //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     //   child.setVelocityX(150 - Math.random() * 300);
@@ -40,11 +47,17 @@ export default new Phaser.Class({
       gridAlign: {width: 3, height: 3, cellWidth: 160, cellHeight: 150, x: 105, y: 260}
     })
     console.log("VIRUSES",viruses)
+
+    //Making viruses interactive
     Phaser.Actions.Call(viruses.getChildren(), function(virus){
       virus.setInteractive();
       virus.on("pointerdown", function(pointer){
-        console.log("VIRUS", virus)
+        //Destroy virus
         virus.destroy();
+        //Add to score
+        score++;
+        console.log("SCORE UPDATE", score)
+        scoreText.setText(`Score: ${score}`)
 
         const virusesLeft = viruses.countActive();
         console.log("Viruses left", virusesLeft)
@@ -52,7 +65,8 @@ export default new Phaser.Class({
           this.scene.start("winscreen");
         }
       }, this);
-    }, this)
+    }, this);
+
 
   },
   update: function () {
