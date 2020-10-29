c997d9bcd1ee6ce44fa056a3e58a14521decd5a6
Victoria <vicglee1234007@gmail.com>
2020-10-29 02:52:36 -0500

    Clean up console.logs & comments

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -39,11 +39,12 @@ export default new Phaser.Class({
 
   },
   create: function create() {
-
+    //Creating game elements
     this.add.image(275, 410, "background"); //note: All Phaser3 Game Obj are positioned based on their center by default, and can be changed to have the drawing position set to the top-left by appending .setOrigin(0,0) to this line
 
     //Score keeping
     scoreText = this.add.text(20,20, "Score: 0", {fontSize: "30px", fill: "#000"})
+
     //Timer
     function formatTime(seconds){
       let min = Math.floor(seconds/60);
@@ -59,30 +60,21 @@ export default new Phaser.Class({
     }
     timeEvent = this.time.addEvent({delay: 1000, callback: decreaseTime, callbackScope: this, loop: true})
 
-
-    // stars.children.iterate(function (child) {
-    //   child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    //   child.setVelocityX(150 - Math.random() * 300);
-    //   child.setVelocityY(150 - Math.random() * 300);
-    //   child.setBounce(1, 1);
-    //   child.setCollideWorldBounds(true);
-    // });
-
+    //Create Viruses
     viruses = this.physics.add.staticGroup({
       key: "virus",
       repeat: 8,
       setScale: { x: 0.75, y: 0.75 },
       gridAlign: {width: 3, height: 3, cellWidth: 160, cellHeight: 150, x: 105, y: 260}
     })
+    //Helps generate random num of viruses
     let randomNum = Math.floor(Math.random()*(8-1)+1);
     for (let i = 0; i < randomNum; i++) {
       let reaper = Math.floor(Math.random()*(8-1)+1);
       viruses.children.entries[reaper].disableBody(true,true);
     }
 
-    console.log("GET CHILDREN",viruses.getChildren())
-    console.log("CHILDREN", viruses.children.entries)
-
+    //Create animations
     this.anims.create({
       key:"neutral",
       frames: [{key: "virus", frame: 0}],
@@ -96,35 +88,24 @@ export default new Phaser.Class({
       duration: 100
     })
 
-    // console.log("VIRUSES",viruses)
-
+    //Kills virus sprite
     const killSprite = function(virus){
       let killedVirus = virus;
           killedVirus.anims.play("hit");
-          // console.log("killed virus", killedVirus)
           setTimeout(function(){
             killedVirus.disableBody(true, true);
-            // disabledBodies.push(killedVirus);
           }, 100)
-          // console.log("disabled ones", disabledBodies)
-
     }
 
     //Making viruses interactive
     Phaser.Actions.Call(viruses.getChildren(), function(virus){
       virus.setInteractive();
+      //When virus is clicked it is killed
       virus.on("pointerdown", function(pointer){
         killSprite(virus);
-        //Add to score
+        //Update score
         score++;
-        // console.log("SCORE UPDATE", score)
         scoreText.setText(`Score: ${score}`)
-
-        // const virusesLeft = viruses.countActive();
-        // console.log("Viruses left", virusesLeft)
-        // if(virusesLeft==0){
-        //   this.scene.start("winscreen");
-        // }
       }, this);
     }, this);
 
@@ -132,20 +113,12 @@ export default new Phaser.Class({
 
   },
   update: function () {
-    // var pointer = this.input.activePointer;
     function getRandom(max, min=0){
       return Math.floor(Math.random()*(max-min)+min)
     }
 
     if((viruses.countActive())<=3){
-      // let disabledLength = disabledBodies.length;
-      // let randomIdx = getRandom(disabledLength)
-      // console.log("reviving",disabledBodies[randomIdx])
-      // let chosen = disabledBodies.splice(randomIdx, 1)[0];
       let dead = viruses.getChildren().filter(virus => !virus.active);
-      // let dead = viruses.getChildren().filter(virus => !virus.active)
-      // console.log("DEAD", dead)
-      // console.log("ALIVE", alive)
       let randomIdx = getRandom(dead.length);
       let chosen = dead[randomIdx];
       console.log("CHOSEN", chosen)
@@ -161,8 +134,6 @@ export default new Phaser.Class({
     }
     //TODO: find way to kill random viruses and make them reappear randomly
     //TODO: find way to track if there's a virus at x,y position
-    // let alive = viruses.getChildren().filter(virus => virus.active)
-    // console.log("alive", alive)
 
 
     // let randomVirus = viruses.getRandomExists();
